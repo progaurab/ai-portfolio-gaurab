62977a4c33c105af0c083aca3cbdc0fedafbf210
Gaurab Kumar <progaurab@gmail.com>
2024-08-04 19:08:49 +0530

    initial commit

---
+++ constant.py
@@ -0,0 +1,29 @@
+info = {
+   "Pronoun": "he/him", 
+   "Name": "Gaurab",
+   "Full_Name": "Gaurab Kumar",
+   "Intro": "A Tech Educator and AI Enthusiast at cognitiveclass.ai",
+   "About":"Hey there, I'm Gaurab! I'm passionate about leveraging the technology to drive meaningful insights and solutions in business. Check out my free/paid courses and projects on Instill Learning: https://instilllearning.com/",
+   "Stackoverflow":"https://stackoverflow.com/users/4188585/gaurab-kumar?tab=profile",
+   "Medium":"https://medium.com/@progaurab/about",
+   "City":"Bangalore, India",
+   "Photo":"""<a href=\"https://www.linkedin.com/in/progaurab/\"><img src=\"https://avatars.githubusercontent.com/u/30452994?s=400&u=40e7472470c28789c17b1128277259df44b6ae72&v=4" width=\"200\"   alt=\"Profile\" title=\"Profile\"></a>""",
+   "Email": "progaurab@gmail.com"
+}
+
+embed_rss= {
+    'rss':"""<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed" 
+        data-rss="https://medium.com/feed/@progaurab"
+        data-maxcols="3" 
+        data-layout="grid"
+        data-poststyle="inline" 
+        data-readmore="Read the rest" 
+        data-buttonclass="btn btn-primary" 
+        data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""
+}
+
+endorsements = {
+    # "img1": "https://user-images.githubusercontent.com/90204593/238169843-12872392-f2f1-40a6-a353-c06a2fa602c5.png",
+    # "img2": "https://user-images.githubusercontent.com/90204593/238171251-5f4c5597-84d4-4b4b-803c-afe74e739070.png",
+    # "img3": "https://user-images.githubusercontent.com/90204593/238171242-53f7ceb3-1a71-4726-a7f5-67721419fef8.png"
+}
